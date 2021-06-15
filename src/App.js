ddc9c7bfcc8e1701801bcac01a7eb41a0537ca63
Kim Beom Jin <planet1995@nate.com>
2021-06-15 21:47:17 +0900

    Create a variable inside a component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import React, { useRef } from 'react'; 
+// useRef는 특정 DOM을 선택하고 싶을때 사용할 수 있지만, 
+// 어떤 변수를 계속 기억하고 싶을때 사용
 import UserList from './UserList';
 
 function App() {
   const name = 'react';
+  const users = [
+    {
+        id: 1,
+        username: 'KBJ',
+        email: 'KBJ@naver.com'
+    },
+    {
+        id: 2,
+        username: 'CHS',
+        email: 'CHS@nate.com'
+    },
+    {
+        id: 3,
+        username: 'JCG',
+        email: 'JCG@hanmail.net'
+    }
+  ];
+
+  // 기본값
+  // nextId를 useRef로 관리하는 이유는 useRef값이 바뀐다고해서 컴포넌트가 리렌더링 될 필요가 없기 때문
+  const nextId = useRef(4); 
+
+  const onCreate = () => {
+    console.log(nextId.current) // 4(현재 userRef값)
+    nextId.current += 1; 
+    // onCreate 함수가 실행될 때마다 기존값에 +1씩 증가
+    // 값이 변경된다고 해서 컴포넌트가 리렌더링 되지 않음
+  }
 
   return (  
-    <UserList />
+    <UserList users={users} />
   );
 }
 
--- src/UserList.js
+++ src/UserList.js
@@ -9,28 +9,9 @@ function User({ user }) {
     );
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'KBJ',
-            email: 'KBJ@naver.com'
-        },
-        {
-            id: 2,
-            username: 'CHS',
-            email: 'CHS@nate.com'
-        },
-        {
-            id: 3,
-            username: 'JCG',
-            email: 'JCG@hanmail.net'
-        }
-    ];
+// UserList를 props로 받음
+function UserList({ users }) {
 
-    // key값이 있어야 정확히 해당 요소가 어떤 데이터를 가르키는지 알 수 있음
-    // 즉 배열을 렌더링 할 때는 key을 설정해야 효율적으로 렌더링 할 수 있음
-    // 만약 고유값이 없는 경우에는 index를 이용해서 사용할 수 있지만 비효율적임
     return (
         <div> 
           {
@@ -43,3 +24,7 @@ function UserList() {
 }
 
 export default UserList;
+
+// key값이 있어야 정확히 해당 요소가 어떤 데이터를 가르키는지 알 수 있음
+// 즉 배열을 렌더링 할 때는 key을 설정해야 효율적으로 렌더링 할 수 있음
+// 만약 고유값이 없는 경우에는 index를 이용해서 사용할 수 있지만 비효율적임
